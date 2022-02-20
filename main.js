dc98324646e4a5566f30a065494a738ccf2ddf82
Michael Putnam <mikeputnam713@gmail.com>
2022-02-20 13:38:35 -0800

    Refactor

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 var cookPot = document.querySelector('.cook-pot');
-var loading = document.querySelector('.loading')
-var currentDish = document.querySelector('.current-dish')
-var clearButton = document.querySelector('.clear-button')
-var randomDish = document.querySelector('.random-dish')
+var loading = document.querySelector('.loading');
+var currentDish = document.querySelector('.current-dish');
+var clearButton = document.querySelector('.clear-button');
+var randomDish = document.querySelector('.random-dish');
 var letsCookButton = document.querySelector('.cook-button');
-var input = document.querySelectorAll(`input[name = 'course']`)
+var input = document.querySelectorAll(`input[name = 'course']`);
 
 //Random number generator:
 
 function random(array) {
 	return Math.floor(Math.random() * array.length);
-}
+};
 // event listeners:
 
-clearButton.addEventListener('click', returnCookPot)
+clearButton.addEventListener('click', returnCookPot);
 
 letsCookButton.addEventListener('click', function() {
 	event.preventDefault()
 	generateRandomDish()
-})
+});
 
 // Functions: 
 
@@ -28,10 +28,10 @@ function generateRandomDish() {
 		if(input[i].checked) {
 			if(input[i].value === 'entire-meal') {
 				unckeckButton(input[i]);
-				entireMeal()
-				loadAnimation()
+				entireMeal();
+				loadAnimation();
 			} else {
-				loadAnimation()
+				loadAnimation();
 				unckeckButton(input[i]);
 				course = eval(input[i].value);
 				currentDish.innerHTML = course[random(course)];
@@ -41,30 +41,30 @@ function generateRandomDish() {
 }
 
 function loadAnimation() {
-	cookPot.classList.add('animation')
-	loading.classList.remove('hidden',)
-	loading.classList.add('animation2')
-	setTimeout(switchPotAndText, 4000)
-}
+	cookPot.classList.add('animation');
+	loading.classList.remove('hidden',);
+	loading.classList.add('animation2');
+	setTimeout(switchPotAndText, 4000);
+};
 
 function entireMeal() {
-	currentDish.innerHTML = `${mains[random(mains)]} with a side of ${sides[random(sides)]} and ${desserts[random(desserts)]} for dessert!`
-}
+	currentDish.innerHTML = `${mains[random(mains)]} with a side of ${sides[random(sides)]} and ${desserts[random(desserts)]} for dessert!`;
+};
 
 function returnCookPot() {
 	cookPot.classList.remove('hidden');
-	randomDish.classList.add('hidden')
-	cookPot.classList.remove('animation')
-	clearButton.classList.add('hidden')
-}
+	randomDish.classList.add('hidden');
+	cookPot.classList.remove('animation');
+	clearButton.classList.add('hidden');
+};
 
 function switchPotAndText() {
-	loading.classList.add('hidden')
+	loading.classList.add('hidden');
 	cookPot.classList.add('hidden');
-	randomDish.classList.remove('hidden')
-	clearButton.classList.remove('hidden')
-}
+	randomDish.classList.remove('hidden');
+	clearButton.classList.remove('hidden');
+};
 
 function unckeckButton(input) {
 	input.checked = false;
-}
+};
